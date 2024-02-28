fd515db039537e026c906caf67f39aaef16d1a0c
be4dev <134231803+be4dev@users.noreply.github.com>
2024-02-28 03:40:06 +0300

    fix

--- app/src/test/java/com/example/mensmorris/CachingTest.kt
+++ app/src/test/java/com/example/mensmorris/CachingTest.kt
@@ -28,7 +28,7 @@ class CachingTest {
     @Test
     fun `cache test`() {
         position.solve(6u)
-        TestCase.assertEquals(occurredPositions.size, 14427)
+        TestCase.assertEquals(occurredPositions.size, 3028)
         TestCase.assertEquals(position.solve(4u).second, null)
         resetAnalyze()
         TestCase.assertEquals(position.solve(4u).second, null)
